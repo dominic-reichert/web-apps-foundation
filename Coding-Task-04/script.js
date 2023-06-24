c97ad52618b9f676ecf33d00727762673c498f47
dominic-reichert <dominicreichert92@gmail.com>
2023-06-24 10:04:48 +0200

    "inital commit"

---
+++ Coding-Task-04/script.js
@@ -0,0 +1,35 @@
+const clickArea = document.querySelector("main");
+const counter = document.getElementById("counter");
+const btn = document.querySelector("button");
+
+let count = 0;
+let bgCount = 0;
+
+btn.addEventListener("click", function () {
+  count = 0;
+  bgCount = 0;
+  counter.textContent = count;
+  clickArea.style.setProperty("background-size", `${bgCount}%`);
+});
+
+clickArea.addEventListener("click", function () {
+  count++;
+  bgCount++;
+  counter.textContent = count;
+  if (bgCount === 101) {
+    bgCount = 1;
+  }
+  clickArea.style.setProperty("background-size", `${bgCount}%`);
+});
+
+document.body.onkeydown = function (e) {
+  if (e.keyCode == 32) {
+    count++;
+    bgCount++;
+    counter.textContent = count;
+    if (bgCount === 101) {
+      bgCount = 1;
+    }
+    clickArea.style.setProperty("background-size", `${bgCount}%`);
+  }
+};
