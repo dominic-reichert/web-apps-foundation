de7e46e63b4088f8e4fe2a81482bd8561e79b15c
dominic-reichert <dominicreichert92@gmail.com>
2023-07-10 14:31:41 +0200

    refactored code

--- Coding-Task-02/script.js
+++ Coding-Task-02/script.js
@@ -1,16 +1,13 @@
 const input = document.querySelector("input");
-
 const btn = document.querySelector("button");
-
-console.log(input.type);
 
 function showPassword() {
   if (input.type === "password") {
     input.setAttribute("type", "text");
-    btn.innerHTML = "Hide Password";
+    btn.innerText = "Hide Password";
   } else if (input.type === "text") {
     input.setAttribute("type", "password");
-    btn.innerHTML = "Show Password";
+    btn.innerText = "Show Password";
   }
 }
 
